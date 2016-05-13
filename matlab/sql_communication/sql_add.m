b8bed788ff8578b6af79c7b17819ca8b9774bbc1
nparzan <parzan@gmail.com>
2016-05-13 15:34:29 +0300

    Remove objective from eeg_activity and support multiple objectives

--- matlab/sql_communication/sql_add.m
+++ matlab/sql_communication/sql_add.m
@@ -1,19 +1,21 @@
 function [ret] = sql_add(varargin)
     constant_load_all;
-    %insert_data_to_sql_url = ;
     activity = varargin{1};
     if strcmp(activity,constant_activity.SESSION_ADD_ACTIVITY)
         subject_id = varargin{2}; experimenter_id = varargin{3}; task = varargin{4};
-        objective_activity_band = varargin{5}; objective_activity_value = varargin{6};
-        electrode_placement = varargin{7}; comment = varargin{8};
-        [ret,data]=create_struct(subject_id,experimenter_id,task,objective_activity_band,objective_activity_value,electrode_placement,comment);
+        objective_delta_value = varargin{5}; objective_theta_value = varargin{6};
+        objective_alpha_value = varargin{7}; objective_beta_value = varargin{8};
+        objective_gamma_value = varargin{9};
+        %objective_activity_band = varargin{5}; objective_activity_value = varargin{6};
+        electrode_placement = varargin{10}; comment = varargin{11};
+        [ret,data]=create_struct(subject_id,experimenter_id,task,objective_delta_value,objective_theta_value,objective_alpha_value,objective_beta_value,objective_gamma_value,electrode_placement,comment);
         request.action = constant_activity.SESSION_ADD_ACTIVITY;
         request.table = constant_table.SESSION;
     elseif strcmp(activity,constant_activity.EEG_ACTIVITY_ADD_ACTIVITY)
-        session_id = varargin{2}; alpha_activity = varargin{3}; beta_activity = varargin{4}; 
-        gamma_activity = varargin{5}; delta_activity = varargin{6}; theta_activity = varargin{7};
-        band_for_improvement = varargin{8}; amplitude_for_improvement = varargin{9};
-        [ret,data]=create_struct(session_id, alpha_activity, beta_activity, gamma_activity, delta_activity, theta_activity, band_for_improvement, amplitude_for_improvement);
+        session_id = varargin{2}; stimulation_id = varargin{3}; 
+        alpha_activity = varargin{4}; beta_activity = varargin{5}; 
+        gamma_activity = varargin{6}; delta_activity = varargin{7}; theta_activity = varargin{8};
+        [ret,data]=create_struct(session_id,stimulation_id, alpha_activity, beta_activity, gamma_activity, delta_activity, theta_activity);
         request.action = constant_activity.EEG_ACTIVITY_ADD_ACTIVITY;
         request.table = constant_table.EEG_ACTIVITY;
     elseif strcmp(activity,constant_activity.STIMULATION_ADD_ACTIVITY)
