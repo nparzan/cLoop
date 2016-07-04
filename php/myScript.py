c164b1ed53a82ca48c7ae42bd706ef2b358953f0
nparzan <parzan@gmail.com>
2016-07-04 12:32:04 +0300

    Remove deprecated matlab add APIs

--- php/myScript.py
+++ php/myScript.py
@@ -6,17 +6,15 @@ def f():
 
 # Load the data that PHP sent us
 try:
-    data = json.loads(sys.argv[1])
-    print("OK")
+    data = int(sys.argv[1])
+    #print("OK")
 except:
     print "ERROR"
     sys.exit(1)
 
 # Generate some data to send to PHP
-result = {'status': 'Yes!'}
+#result = {'status': 'Yes!'}
 
 # Send it to stdout (to PHP)
-print json.dumps(result)
+print data * 2 #json.dumps(result)
 
-print("XYZ")
-f()
--- php/run_py.php
+++ php/run_py.php
@@ -1,56 +1,17 @@
 <?php
 //header('Content-type: application/json');
 
-// Setup parameters
-$servername = "mysqlsrv.cs.tau.ac.il";
-$username = "noamp1";
-$password = "noa7811";
-$dbname = "noamp1";
-$low = $_POST["LowID"];
-$high = $_POST["HighID"];
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// Construct query
-$sql = "SELECT * FROM test where id >= $low AND id <= $high";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-	$arr = array();
-    while($row = $result->fetch_assoc()) {
-		$arr[] = $row;
-        //echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-//$j_arr = json_encode($arr);
-$cmd = 'python myScript.py ' . escapeshellarg(json_encode($arr));
+$cmd = 'python myScript.py ' . escapeshellarg(10);
 $ret = shell_exec($cmd);
 
 echo "\n";
 echo "Return raw:\n";
 print_r($ret);
 echo "\n";
-// Decode the result
-$resultData = json_decode($ret, true);
 
-// This will contain: array('status' => 'Yes!')
-echo "About to print return value12323\n";
-var_dump($resultData);
 
-//print_r($out);
 
 echo "\n";
-
-$conn->close();
 
 ?>
 
